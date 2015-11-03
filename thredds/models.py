7413637e69a647507c53e87ef5c03e40c123a05b
Scott Wales <scott.wales@unimelb.edu.au>
2015-11-03 16:59:11 +1100

    Basic model

--- thredds/models.py
+++ thredds/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Dataset(models.Model):
+    """
+    A dataset on the remote Thredds server
+    """
+    url = models.CharField()
